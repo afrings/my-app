792944cee8a3ede5913fc19b6bc115ed51fe38ad
afrings <afrings@wisc.edu>
2022-06-06 13:31:34 -0500

    changed main page image and description

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './imgs/Abby_&_Austin_UW_Madison_30.jpg';
 import './App.css';
 
 function App() {
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          tall, lanky mofo
         </p>
       </header>
     </div>
